6400991131faee2f0720c07cd25df66f026cdebe
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-02-17 18:31:12 +0100

    Initial code commit

---
+++ etc/di/buzz.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+use Clue\React\Buzz\Browser;
+use React\Dns\Resolver\Resolver;
+use React\EventLoop\LoopInterface;
+use React\Socket\Connector;
+use function DI\factory;
+
+return [
+    Browser::class => factory(function (
+        Resolver $resolver,
+        LoopInterface $loop
+    ) {
+        return new Browser(
+            $loop,
+            new Connector(
+                $loop,
+                $resolver
+            )
+        );
+    }),
+];
